a8065318e5c5c76cc573f33f41bebceaa78f7d38
Kauã de Souza da Silva <kaua.jesuscristo78@gmail.com>
2026-05-22 00:45:35 -0300

    atualizando jQuery para versão 4.0.0 e adicionando novas funcionalidades ao script

--- lista02 - jquery/js/script.js
+++ lista02 - jquery/js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+    // Questão 1
     // A
     $(".module")
     
@@ -20,5 +21,45 @@ $(function() {
 
     // G
     $("#myList li:not(#myList li:['id = 'myListItem''])")
+
+    // H
+    $("img:last, h3:last")
+
+    // I
+    $("#myList li[class='current bar'] ~ li")
+
+    // Questão 2
+    // A
+    for (var i=8; i<13; i++) {
+        $("#myList").append(`<li>List item ${i}</li>`)
+    }
+
+    // B
+    $("#myList li:odd").remove()
+
+    // C
+    $("div[class='module']:last").append(`
+        <div class='module'>
+            <img src="images/fruit.jpg">
+        </div>
+    `)
+
+    // Questão 3
+    // A
+    $("img[alt]").each(function() {
+        console.log($(this));
+    });
+
+    // B e C
+    $("input[type='text']").closest("form").addClass("template");
+
+    // D, E, F e G
+    $("#myList li.current").removeClass("current").next().addClass("current");
+
+    // H
+    $("#specials h2").text("Promoções").parent().find("option:contains('Friday')").text("Dimitri");
+
+    // I
+    $("#slideshow li:first-child").addClass("current").siblings().addClass("disabled");
 })
 
